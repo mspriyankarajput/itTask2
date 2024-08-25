619bf6fff016e79bfcff54d72562f7849475639e
Priyanka <rk26684790@gmail.com>
2024-08-25 11:42:00 +0530

    These files describe the code for basic calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
